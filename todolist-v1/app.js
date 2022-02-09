ed5a4b8bac664558e06984be8679bd8c4a39d960
minTwin <michaelmin2020@gmail.com>
2022-02-09 17:32:29 -0600

    Implemented css styling

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,29 +5,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Improve ASP.net C# programming skills", "Improve Vue programming skills", "Eat Food"];
+let items = ["Improve ASP.net C# programming skills", "Improve Vue programming skills", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extened: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {kindOfDay: day, newListItems: items});
 
 
     app.post("/", function (req, res) {
-        var item =req.body.newItem;
+        let item =req.body.newItem;
 
         items.push(item);
 
