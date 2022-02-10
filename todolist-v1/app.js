3f544b2266cec42859abe9e2812e582116bddda3
minTwin <michaelmin2020@gmail.com>
2022-02-10 10:44:06 -0600

    implemented templating and layouts

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,37 +5,49 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let items = ["Improve ASP.net C# programming skills", "Improve Vue programming skills", "Eat Food"];
+let items = ["Improve ASP.net C# programming skills", "Improve Vue/Javascript programming skills", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extened: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    let today = new Date();
+        let today = new Date();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+        let options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+        };
 
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay: day, newListItems: items});
-
+        let day = today.toLocaleDateString("en-US", options);
+        res.render("list", {listTitle: day, newListItems: items});
+    });
 
     app.post("/", function (req, res) {
-        let item =req.body.newItem;
+        let item = req.body.newItem;
 
-        items.push(item);
+        if (req.body.list === "Work") {
+            workItems.push(items);
+            res.redirect("/work");
+        }
+        else {
+            items.push(item);
+            res.redirect("/");
+        }
+    });
 
-        res.redirect("/");
+    app.get("/work", function(req,res) {
+        res.render("list", {listTitle: "Work List", newListItems: workItems});
+    });
+
+    app.get("/about", function(req, res) {
+        res.render("about");
     })
 
-});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
