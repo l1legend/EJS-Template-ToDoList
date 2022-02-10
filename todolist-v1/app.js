717f381f6d34f97b13e51b925287d17fc80625e7
minTwin <michaelmin2020@gmail.com>
2022-02-10 13:09:02 -0600

    modularized function to their own file/class
    date.js functions are called in app.js

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,11 +2,16 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+console.log(date);
 
 const app = express();
 
-let items = ["Improve ASP.net C# programming skills", "Improve Vue/Javascript programming skills", "Eat Food"];
-let workItems = [];
+const items = ["Improve ASP.net C# programming skills", "Improve Vue/Javascript programming skills", "Eat Food"];
+const workItems = [];
+
+console.log(workItems);
 
 app.set('view engine', 'ejs');
 
@@ -15,38 +20,33 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-        let today = new Date();
-
-        let options = {
-            weekday: "long",
-            day: "numeric",
-            month: "long"
-        };
-
-        let day = today.toLocaleDateString("en-US", options);
-        res.render("list", {listTitle: day, newListItems: items});
+    const day = date.getDate;
+    //let day = date.getDay;
+    res.render("list", {listTitle: day, newListItems: items});
     });
 
-    app.post("/", function (req, res) {
-        let item = req.body.newItem;
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
 
-        if (req.body.list === "Work") {
-            workItems.push(items);
-            res.redirect("/work");
-        }
-        else {
-            items.push(item);
-            res.redirect("/");
-        }
-    });
+    if (req.body.list === "Work") {
+        workItems.push(items);
+        console.log(workItems);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
 
-    app.get("/work", function(req,res) {
-        res.render("list", {listTitle: "Work List", newListItems: workItems});
-    });
+app.get("/work", function(req,res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+    
+});
 
-    app.get("/about", function(req, res) {
-        res.render("about");
-    })
+app.get("/about", function(req, res) {
+    res.render("about");
+})
 
 
 app.listen(3000, function () {
